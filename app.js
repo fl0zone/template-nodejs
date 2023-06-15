b19e2de29511416a6f30faa697e4c61cb0644135
james-fl0 <88681427+james-fl0@users.noreply.github.com>
2023-06-15 12:29:48 -0700

    Update app.js
    Signed-off-by: james-fl0 <88681427+james-fl0@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 1111;
-// const databaseURL = process.env.DATABASE_URL || (() => { throw new Error('Could not connect to database'); })();
 
 app.use(express.static('public'))
 
-app.get('*', (req, res) => {
+app.get('*, (req, res) => {
     res.redirect('/');
 })
 
