e6d42cc10e8df1315aa8781beeae9d5ad849dda5
james-fl0 <88681427+james-fl0@users.noreply.github.com>
2023-07-05 17:41:37 -0700

    Update app.js
    Signed-off-by: james-fl0 <88681427+james-fl0@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT ?? 3000;
+if(typeof process.env.PORT === 'undefined') throw new Error();
+const port = process.env.PORT;
 
 app.use(express.static('public'))
 
