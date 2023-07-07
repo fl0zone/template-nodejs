c3f26706e200337f802c94c03176553e0f6ca276
James Harrison <james@fl0.com>
2023-07-07 11:19:39 -0600

    Commiting eslint config with intentionally broken formatting to test buildpack

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
+
 const app = express();
 const port = process.env.PORT ?? 3000;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('*', (req, res) => {
     res.redirect('/');
-})
+});
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+  console.log(`App listening on port ${port}`); // eslint-disable-line
+});
