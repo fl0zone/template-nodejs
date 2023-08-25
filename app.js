1127aa8844031eb8f8b3b9a89bf0803c9d7c67a5
cindersax <saporrasfo@unal.edu.co>
2023-08-25 18:19:50 -0500

    test scrape endpoint

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-const express = require('express');
+import express from "express";
+import puppeteer from "puppeteer";
 const app = express();
-const port = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 3000;
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('*', (req, res) => {
-    res.redirect('/');
-})
+app.get("/scrape", async (req, res) => {
+  let browser;
+  try {
+    browser = await puppeteer.launch({
+      headless: "new",
+    });
+    const page = await browser.newPage();
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+    await page.goto("https://traders.studio/");
+
+    const result = await page.$eval(".brxe-post-title", (el) => el.innerText);
+
+    res.json({ title: result });
+  } catch (error) {
+    res.status(500).send(`Error al extraer el contenido: ${error.toString()}`);
+  } finally {
+    if (browser) await browser.close();
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
