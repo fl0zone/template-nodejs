83572a207d11094f9be0af0c3040d9ca0541ba5a
Damir <damir@t1c.ru>
2023-10-10 21:34:02 +0300

    тестовая страница + nodemon для разработки

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const port = process.env.PORT ?? 3000;
 
 app.use(express.static('public'))
 
-app.get('*', (req, res) => {
-    res.redirect('/');
+app.get('/home', (req, res) => {
+    res.redirect('/')
 })
+
+app.get('/test', (req, res) => {
+    res.send("<p>Тестовая страница</p> <p><a href=\"/home\">Вернуться на главную</a></p>")
+})
+
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
