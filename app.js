e8669e93e3e8361bdfef3b3734a532ab1a81e917
Agrosistemas Web <113381383+matias-gumma-agrosistemas@users.noreply.github.com>
2023-10-19 14:53:25 -0300

    :id

--- app.js
+++ app.js
@@ -31,7 +31,34 @@ app.get('/articulos', async (req, res) => {
       console.error(error);
       res.status(500).send('Internal Server Error');
     }
-  });
+});
+
+app.get('/articulos/cuenta/:id', async (req, res) => {
+  try {
+    // Create a connection pool
+    const pool = await sql.connect({
+      server: '179.43.116.142',
+      database: 'PuestoLob_Pick',
+      user: 'qq',
+      password: 'qq11',
+      port: 1433,
+      options: {
+        trustedConnection: true,
+        encrypt: false,
+        trustServerCertificate: true,
+      },
+    });
+
+    // Execute a query
+    const result = await pool.request().query(`SELECT * FROM M6_Picking where IDCtaCte = ${id}`);
+
+    // Send the result as a response
+    res.json(result.recordset);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
