106c37caa0970a1115ffbda1a9a15e4831e18a7c
Agrosistemas Web <113381383+matias-gumma-agrosistemas@users.noreply.github.com>
2023-10-19 13:45:32 -0300

    d

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require('express');
+const sql = require('mssql');
 const app = express();
 const port = process.env.PORT ?? 3000;
 
 app.use(express.static('public'))
 
-app.get('*', (req, res) => {
-    res.redirect('/');
-})
+// app.get('*', (req, res) => {
+//     res.redirect('/');
+// })
+
+app.get('/api/endpoint', async (req, res) => {
+    try {
+      // Create a connection pool
+      const pool = await sql.connect({
+        server: '179.43.116.142',
+        database: 'PuestoLob_Pick',
+        user: 'qq',
+        password: 'qq11',
+        port: 1433,
+        options: {
+          enableArithAbort: true,
+        },
+      });
+
+      // Execute a query
+      const result = await pool.request().query('SELECT * FROM YourTable');
+
+      // Send the result as a response
+      res.json(result.recordset);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
