2eb5aa8f17b062741147f22cfb22cc8cbf0c0552
Victor261995 <mad_civ@yahoo.com>
2023-12-07 20:28:12 -0300

    trying3

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT ?? 3000;
+
+app.use(express.static('public'))
+
+app.get('*', (req, res) => {
+    res.redirect('/');
+})
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+})
