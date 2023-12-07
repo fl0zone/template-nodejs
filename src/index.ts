9eee4179777137ecbc6e24328f125d8eedae1e8b
Victor261995 <mad_civ@yahoo.com>
2023-12-07 20:19:18 -0300

    trying2

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+
+
+const prisma= new PrismaClient();
+
+const app=express();
+
+const port = process.env.PORT ?? 8080 ;
+app.use(express.json());
+
+
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+  });
