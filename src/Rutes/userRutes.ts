c03f059192291de499349cb7b0074314852225bc
Santiago <79413503+santiagoweb212@users.noreply.github.com>
2024-01-17 19:03:55 -0500

    Add Validation Middleware for User Registration (#1)
    * add: express validator
    
    * add: middleware for sign up field validation
    Details:
    - Added a new middleware module for sign-up field validation
    - Implemented checks for required fields and data format
    - Updated the sign-up route to utilize the new validation middleware

--- src/Rutes/userRutes.ts
+++ src/Rutes/userRutes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import{createUser} from"../Controllers/UsersControllers-Grupo A"
+import validateFieldSignUp from "../validator/validateFieldSignUp";
 
 export const userRouter=Router();
 
-userRouter.post('/NewUser',createUser);
+userRouter.post('/NewUser',validateFieldSignUp,createUser);
